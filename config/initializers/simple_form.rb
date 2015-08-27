e999dba68f04110bba1f94cc28c78a61af44983a
Charles Kim <ckim0706@gmail.com>
2015-08-27 14:50:12 +0900

    Change front-end layout and add custom css

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,6 +5,7 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
+  
   config.wrappers :default, class: :input,
     hint_class: :field_with_hint, error_class: :field_with_errors do |b|
     ## Extensions enabled by default
@@ -20,6 +21,7 @@ SimpleForm.setup do |config|
     # Calculates placeholders automatically from I18n
     # You can also pass a string as f.input placeholder: "Placeholder"
     b.use :placeholder
+
 
     ## Optional extensions
     # They are disabled unless you pass `f.input EXTENSION_NAME => true`
