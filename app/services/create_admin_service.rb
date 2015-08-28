b7536aa6f1a245f243f215facb16529d50c67071
Charles Kim <ckim0706@gmail.com>
2015-08-28 10:47:30 +0900

    Fixed minor errors for heroku

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -2,7 +2,7 @@ class CreateAdminService
   def call
     user = User.find_or_create_by!(email: ENV['gmail_username']) do |user|
         user.password = ENV['admin_password']
-        user.password_confirmation = ENV['gmail_username']
+        user.password_confirmation = ENV['admin_password']
         user.admin!
       end
   end
