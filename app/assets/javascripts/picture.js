33a67e769c8ad896395b368b32b24476df9d98fc
Charles Kim <ckim0706@gmail.com>
2015-08-30 15:18:04 +0900

    Fixed paperclip styles

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-  $("a.fancybox").fancybox();
-});
+
+  // if (window.width < 768) {
+   
+    $("a.fancybox")
+      .attr('rel', 'gallery')
+      .fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+        nextEffect  : 'elastic',
+        prevEffect  : 'elastic',
+        margin      : 50,
+        helpers : {
+          title : { type : 'inside' }
+        }, // helpers
+        beforeShow : function() {
+          this.title = (this.title ? '' + this.title + '' : '') + ' ' + (this.index + 1) + ' / ' + this.group.length;
+        } // beforeShow
+    }); //fancybox
+  // } // if
+}); //ready
 
 // $(document).ready(function(){
 //    $('.picture img').on('click',function(e){
--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,7 +1,7 @@
 module AlbumsHelper
   def album_thumbnail(album)
     if album.pictures.count > 0
-      image_tag(album.pictures.first.asset.url(:large))
+      image_tag(album.pictures.first.asset.url(:album))
     else
       image_tag("http://placehold.it/800x800")
     end
