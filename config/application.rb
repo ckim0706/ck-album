9fe4dd956f87c0f5018cab828708a7f86d4bc129
Charles Kim <ckim0706@gmail.com>
2015-09-01 14:33:02 +0900

    changed name of app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Hana
+module Ckim
   class Application < Rails::Application
 
     config.generators do |g|
