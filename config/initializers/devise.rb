e07657dc32247a53953b160df5a7ece6d1a5493b
Charles Kim <ckim0706@gmail.com>
2015-09-03 13:47:07 +0900

    heroku css fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,4 +10,5 @@ Devise.setup do |config|
   config.password_length = 8..72
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+  config.secret_key = '2a835a6478fe7f546a9d53b32d2b040f6603ae6c60b951a184103e580c4c3687c3dc7f4f8d92b3eda2bed88581be64fbd3c979cbc44059edb007caafc51ebfc0'
 end
